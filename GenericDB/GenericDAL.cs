2f306eeb50d3492b2ef67abc123a26deb669297c
Yan Zhang <yan_zhang@bcbsil.com>
2018-11-04 14:18:58 -0600

    Add project files

---
+++ GenericDB/GenericDAL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace GenericDB
+{
+    public class GenericDAL
+    {
+        public static DataTable ReadTable(string query, params object[] args)
+        {
+            var dataTable = new DataTable();
+
+            using (AdoHelper db = new AdoHelper("14a_Op_Intel", true))
+            using (SqlDataReader rdr = db.ExecDataReader(query, args))
+            {
+                dataTable.Load(rdr);
+            }
+            return dataTable;
+        }
+
+
+        public static DataTable ReadStoredProc(string query, params object[] args)
+        {
+            var dataTable = new DataTable();
+
+            using (AdoHelper db = new AdoHelper("14a_Op_Intel", true))
+            using (SqlDataReader dr = db.ExecDataReaderProc(query, args))
+            {
+                dataTable.Load(dr);
+            }
+            return dataTable;
+        }
+
+        public static DataTable ReadStoredProcFromList(string query, List<string> args)
+        {
+            var dataTable = new DataTable();
+
+            using (AdoHelper db = new AdoHelper("14a_Op_Intel", true))
+            using (SqlDataReader dr = db.ExecDataReaderProcGeneric(query, args))
+            {
+                dataTable.Load(dr);
+            }
+            return dataTable;
+        }
+
+
+    }
+}
---
+++ GenericDB/Program.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ReadStoredProcExample();
+            ReadStoredProcWebRequestExample();
+
+            Console.ReadKey();
+        }
+
+        public static List<string> ConstructArgs(NameValueCollection myCol)
+        {
+            List<string> args = new List<string>();
+
+            foreach (string k in myCol.AllKeys) // skip 1 if first one is stored proc name
+            {
+                var key = "@" + k;
+                var value = myCol[k];
+                args.Add(key);
+                args.Add(value);
+            }
+            return args;
+        }
+
+
+        public static void ReadStoredProcWebRequestExample()
+        {
+            string name = "GetUserByName";
+
+            // simulating QueryString in web request  var parameters = Request.QueryString;
+            NameValueCollection myCol = new NameValueCollection();
+            myCol.Add("FirstName", "Yan");
+            myCol.Add("LastName", "Zhang");
+            myCol.Add("Theme", "1");
+
+            var args = ConstructArgs(myCol);
+
+            var data = GenericDAL.ReadStoredProcFromList(name, args);
+
+            string json = JsonConvert.SerializeObject(data);
+
+            Console.WriteLine(json);
+        }
+
+        public static void ReadStoredProcExample()
+        {
+            string name = "GetUserByName";
+
+            var data = GenericDAL.ReadStoredProc(name, "@FirstName", "Yan", "@LastName", "Zhang", "@Theme", "1");
+
+            string json = JsonConvert.SerializeObject(data);
+
+            Console.WriteLine(json);
+        }
+
+        public static void ReadTableExample()
+        {
+            string qry = "Select * FROM [Op_Intel].[pnr].[WebUsers] where [FirstName] =@FirstName and [LastName] = @LastName ";
+
+            var data = GenericDAL.ReadTable(qry, "@FirstName", "Yan", "@LastName", "Zhang");
+
+            string json = JsonConvert.SerializeObject(data);
+
+            Console.WriteLine(json);
+        }
+    }
+}
